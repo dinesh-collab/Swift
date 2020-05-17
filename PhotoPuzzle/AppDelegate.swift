bc5a0a6b2ce95d996ba84a297b07a0088d0febd3
dinesh-collab <yeligindladinesh@gmail.com>
2020-05-16 20:13:52 -0700

    Swift file Photo Puzzle Updated

---
+++ PhotoPuzzle/AppDelegate.swift
@@ -0,0 +1,72 @@
+//
+//  AppDelegate.swift
+//  PhotoPuzzle
+//
+//  Created by Vaideeswaran Kaliannan on 10/15/2019.
+//  Copyright © 2019 Vaideeswaran Kaliannan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        // define user defaults
+        let userDefaults = UserDefaults.standard
+        
+        
+        
+        // get today's date formatted
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        var initialLaunch = formatter.string(from: date)
+
+        // update existing launch date if its older then today or doesn't exist
+        if let existingLaunch: String = userDefaults.string(forKey: "initial_launch") {
+            if (Int(existingLaunch)! < Int(initialLaunch)!) {
+                initialLaunch = existingLaunch
+            }
+        }
+        
+        // Register default values for settings
+        let defaults: [String: Any] = ["initial_launch" : initialLaunch, "camera_enabled_preference": true,  "photo_library_enabled_preference" : true]
+        
+        userDefaults.register(defaults: defaults)
+        userDefaults.synchronize()
+        print(userDefaults.dictionaryRepresentation())
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
